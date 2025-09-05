150589dd570bb7b793fdd000673d8fd2e212230c
tusharrajput7029 <tusharrajput7129@gmail.com>
2025-09-05 10:02:18 +0530

    first

--- main.js
+++ main.js
@@ -2,9 +2,9 @@
 
     const questions = [
       {
-        question: "What is the capital of France?",
-        options: ["Berlin", "Madrid", "Paris", "Rome"],
-        answer: "Paris"
+        question: "What is the capital of India?",
+        options: ["Berlin", "Madrid", "New Delhi", "Rome"],
+        answer: "New Delhi"
       },
       {
         question: "Which language runs in a web browser?",
